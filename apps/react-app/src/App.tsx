271f3bd7152d05b0f8935d126592d35e05a2a66f
AyushRay06 <rayayush47@gmail.com>
2024-04-07 15:05:36 +0530

    updated package.json

--- apps/react-app/src/App.tsx
+++ apps/react-app/src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
+import { Button } from "@repo/ui/button"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        <Button appName="react-app">Hello From React</Button>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
